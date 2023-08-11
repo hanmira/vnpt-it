19d1f57c198fc0a4b864e76723f121855d326a04
hanmira <vothanhngan599@gmail.com>
2023-08-12 00:44:23 +0700

    finished

--- js/header.js
+++ js/header.js
@@ -4,12 +4,9 @@ document.querySelector(".menu-icon").addEventListener("click", function () {
   document.querySelector(".menu").classList.toggle("open");
 });
 
-//loop qua cac link cua header
-//check if cai link nao tren html click thi get id trong cai link
-//loop qua cac link trong tab gt.html
-//if get id clicked === id tab thi active tab
-//??????
-//tao 1 bien
-//get id cua clicked header gan cho 1 biến neu khac null
-//loop qua tab link trong gt.html
-//tab link id === clicked header id thi active tab?????????????????
+document.querySelectorAll(".nav-link-disabled").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+  console.log(link);
+});
